44c0c78dbd34bb0ec433d317c27acc7744e24dfe
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 14:39:46 +0600

    added header for searchMeal

---
+++ src/components/searchMeal/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Index;
