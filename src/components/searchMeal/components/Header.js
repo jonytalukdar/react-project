4d58eceb419043c100cf966ca531647852e7722c
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 15:56:52 +0600

    added api key id

--- src/components/searchMeal/components/Header.js
+++ src/components/searchMeal/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Header = () => {
-  return (
-    <div>
-      <h1>this is header</h1>
-    </div>
-  );
+  const APP_ID = '33aef683';
+  const APP_KEY = 'e6754b9846a6dbc49dfb5c9d0d18f788';
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return <div></div>;
 };
 
 export default Header;
