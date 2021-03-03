8a882ee0bf41a831a1910cad45101a069dcdfb9b
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 19:26:33 +0600

    added submit button for search meal

--- src/components/searchMeal/components/Header.js
+++ src/components/searchMeal/components/Header.js
@@ -4,23 +4,27 @@ import Recipe from './Recipe';
 const Header = () => {
   const [recipe, setRecipe] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('biryani');
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
       .then((response) => response.json())
       .then((data) => setRecipe(data.meals));
-  }, []);
+  }, [query]);
 
   const updateChange = (e) => {
     setSearch(e.target.value);
-    // console.log('helo');
-    console.log(search);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={getSearch}>
         <input type="text" value={search} onChange={updateChange} />
         <button type="submit">Seacrh</button>
       </form>
