99f80928d84e351b96ee99f4865367b2ac8f0f7a
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-04 15:31:30 +0600

    create api with useEffect

--- src/components/breakingBad/Index.js
+++ src/components/breakingBad/Index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Header from './component/header/Header';
 
-const index = () => {
+const Index = () => {
+  const [item, setItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItem(result.data);
+      setIsLoading(false);
+    };
+    fetchItem();
+  }, []);
   return (
-    <div>
-      <h1>hello this is breaking bad</h1>
+    <div className="container">
+      <Header></Header>
+      {item.map((list) => (
+        <li>{list.name}</li>
+      ))}
     </div>
   );
 };
 
-export default index;
+export default Index;
