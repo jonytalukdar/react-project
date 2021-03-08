1e14d9384860b189d41b33db4acca3aa269749b6
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-08 23:58:39 +0600

    added image

--- src/components/movie/AllMovies.js
+++ src/components/movie/AllMovies.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 const AllMovies = ({ movie }) => {
-  const { id, name, backdrop_path } = movie;
+  const { id, title, backdrop_path } = movie;
   return (
     <div className="col-md-4 my-2">
       <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img
+          variant="top"
+          src={`https://image.tmdb.org/t/p/w1280/${backdrop_path}`}
+        />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{title}</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
