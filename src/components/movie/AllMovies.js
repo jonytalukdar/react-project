539a5cf21b1c62cf8f858f0ab58c58198f39c59f
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-08 23:59:32 +0600

    added image

--- src/components/movie/AllMovies.js
+++ src/components/movie/AllMovies.js
@@ -12,10 +12,6 @@ const AllMovies = ({ movie }) => {
         />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
